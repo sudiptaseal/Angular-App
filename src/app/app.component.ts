7523f09510c8bf493d8fdc6bbb25c743a0afbdfe
sudipta <sudipta_seal08@yahoo.com>
2024-12-02 09:21:32 +0530

    App Component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from '../navbar/navbar.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
