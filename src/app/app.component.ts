029cd4f3f3488ac7a5b53b169f01205345374bdd
sudipta <sudipta_seal08@yahoo.com>
2024-12-02 10:53:05 +0530

    property binding, class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,9 @@ import { HeaderComponent } from '../header/header.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'This is my first angular app.';
+  imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Satyajit_Ray_in_New_York_%28cropped%29.jpg' 
+  isDisabled = true;
+  isActive : boolean = false;
+  fruitName : String = 'apple';
 }
